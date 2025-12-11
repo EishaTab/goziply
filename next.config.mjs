b69d7a8e8c706ec76accbb951d52297ef52a5dfc
Eisha <71912680+EishaTab@users.noreply.github.com>
2025-12-11 04:25:46 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require('path');
+
+const nextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'app'), // 'app' ko apne directory ke hisaab se adjust karein
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
