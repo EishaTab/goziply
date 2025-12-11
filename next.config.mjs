cb31be9c535a865ee14af67c223eaf9fdd55695f
Eisha <71912680+EishaTab@users.noreply.github.com>
2025-12-11 04:28:38 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@ import path from 'path';
 
 const nextConfig = {
   webpack(config) {
+    const __dirname = path.dirname(new URL(import.meta.url).pathname);  // Replacing __dirname for ES modules
+    
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'app'), // Adjust 'app' to your project directory
+      '@': path.resolve(__dirname, 'app'),  // Adjust 'app' to your project directory
     };
     return config;
   },
